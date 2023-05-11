50a8c8da625cdc7cb3e7de12832a5fa79164144a
FagnerAureliano <fagner2aureliano@gmail.com>
2023-05-11 15:14:39 -0300

    init project and impl module search materials

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import(
+        './modules/search-materials/search-materials.module'
+      ).then((m) => m.SearchMaterialsModule),
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
