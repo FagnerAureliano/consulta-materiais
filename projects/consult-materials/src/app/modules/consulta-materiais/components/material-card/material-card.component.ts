ee4e99a350a3876c146757fd2ed03f6e05dd6428
FagnerAureliano <fagner2aureliano@gmail.com>
2023-06-13 15:27:43 -0300

    feat(project): update guia cadast

--- projects/consult-materials/src/app/modules/consulta-materiais/components/material-card/material-card.component.ts
+++ projects/consult-materials/src/app/modules/consulta-materiais/components/material-card/material-card.component.ts
@@ -3,25 +3,23 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 @Component({
   selector: 'app-cards',
   templateUrl: './material-card.component.html',
-  styleUrls: ['./material-card.component.scss']
+  styleUrls: ['./material-card.component.scss'],
 })
 export class MaterialCardsComponent implements OnInit {
-
   @Input() search!: any;
   @Output() emitTag: EventEmitter<any> = new EventEmitter();
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-    this.search.description = this.truncateString(this.search.description, 150)
-    
+    console.log(this.search);
+
+    this.search.description = this.truncateString(this.search.description, 150);
   }
   truncateString(str: string, num: number) {
     if (str.length <= num) {
-      return str
+      return str;
     }
-    return str.slice(0, num) + '...'
+    return str.slice(0, num) + '...';
   }
-
-
 }
