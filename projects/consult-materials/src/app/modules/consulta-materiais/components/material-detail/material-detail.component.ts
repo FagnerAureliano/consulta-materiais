b7927f4cf47f4b38266b1ce72a13c30f3fbe8343
FagnerAureliano <fagner2aureliano@gmail.com>
2023-07-11 14:44:28 -0300

    :construction: impl download file

--- projects/consult-materials/src/app/modules/consulta-materiais/components/material-detail/material-detail.component.ts
+++ projects/consult-materials/src/app/modules/consulta-materiais/components/material-detail/material-detail.component.ts
@@ -4,11 +4,9 @@ import {
   SafeHtml,
   SafeResourceUrl,
 } from '@angular/platform-browser';
-import { Material } from 'projects/consult-materials/src/app/models/search.models';
 import { ConsultaMateriaisService } from 'projects/consult-materials/src/app/services/consulta-materiais.service';
-import { getFileTypeByMIME } from 'projects/shared/src/lib/utils/file-types';
-import { Subscription, of } from 'rxjs';
-import { catchError, finalize, map, tap } from 'rxjs/operators';
+import { Subject, Subscription, of, throwError } from 'rxjs';
+import { catchError, finalize, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-material-detail',
@@ -18,54 +16,82 @@ import { catchError, finalize, map, tap } from 'rxjs/operators';
 export class MaterialDetailComponent implements OnInit, OnDestroy {
   private _subs$: Subscription[] = [];
   @Input() documentId: any;
-  
+
   visible: boolean;
   mimeType: string;
-  data: SafeHtml;
-  fileURL: SafeResourceUrl;
-  material: Object;
+  note_material: SafeHtml;
+  file_material: SafeResourceUrl;
+  material: any;
+  hasPermission: boolean = true;
 
   constructor(
     private service: ConsultaMateriaisService,
     private sanitizer: DomSanitizer
   ) {}
+
   ngOnDestroy(): void {
     this._subs$.forEach((subs) => subs.unsubscribe());
   }
 
   ngOnInit(): void {}
   handleDownload(): void {
-    console.log('tested download');
+    this._subs$.push(
+      this.service
+        .getDocumentFile(this.documentId)
+        .pipe(
+          catchError((err) => throwError(err)),
+          tap((res) => {
+            const blob = new Blob([res], {
+              type: this.material.properties['file:content']['mime-type'],
+            });
+            const url = window.URL.createObjectURL(blob);
+            const a = document.createElement('a');
+            a.href = url;
+            a.download = this.material?.title;
+            document.body.appendChild(a);
+            a.click();
+            URL.revokeObjectURL(url);
+          }),
+          finalize(() => {
+            const a = document.querySelector('a');
+            if (a) {
+              document.body.removeChild(a);
+            }
+          })
+        )
+        .subscribe()
+    );
   }
+
   showDialog(): void {
     this._subs$.push(
       this.service
-      .getDocumentByID(this.documentId)
-      .pipe(
-        tap((res: any) => {
-          if (res.type != 'Note') {
-            this.mimeType = res.properties['file:content']['mime-type'];
-            this.service.getDocumentFile(res.id).subscribe((file: any) => {
-              const blob = new Blob([file], {
-                type: this.mimeType,
+        .getDocumentByID(this.documentId)
+        .pipe(
+          tap((res: any) => {
+            if (res.type != 'Note') {
+              this.mimeType = res.properties['file:content']['mime-type'];
+              this.service.getDocumentFile(res.id).subscribe((file: any) => {
+                const blob = new Blob([file], {
+                  type: this.mimeType,
+                });
+                const url = window.URL.createObjectURL(blob);
+                this.file_material =
+                  this.sanitizer.bypassSecurityTrustResourceUrl(url);
               });
-              const url = window.URL.createObjectURL(blob);
-              this.fileURL = this.sanitizer.bypassSecurityTrustResourceUrl(url);
-            });
-          }
-        }),
-        catchError(() => of(null)),
-        finalize(() => {
-          this.visible = true;
+            }
+          }),
+          catchError(() => of(null)),
+          finalize(() => {
+            this.visible = true;
+          })
+        )
+        .subscribe((response) => { 
+          this.material = response;
+          this.note_material = this.sanitizer.bypassSecurityTrustHtml(
+            response.properties['note:note']
+          );
         })
-      )
-      .subscribe((response) => {
-        console.log(response);
-        this.material = response;
-        this.data = this.sanitizer.bypassSecurityTrustHtml(
-          response.properties['note:note']
-        );
-      })
     );
   }
 }
--- projects/consult-materials/src/app/modules/consulta-materiais/consulta-materiais.module.ts
+++ projects/consult-materials/src/app/modules/consulta-materiais/consulta-materiais.module.ts
@@ -4,19 +4,19 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import localePt from '@angular/common/locales/pt';
 
-import { TagInputModule } from 'projects/shared/src/lib/components/tag-input/tag-input.module';
-import { LoadingBarModule } from 'projects/shared/src/lib/components/loading-bar/loading-bar.module';
+import { ConsultaMateriaisRoutingModule } from './consulta-materiais-routing.module';
 import { MaterialCardsComponent } from './components/material-card/material-card.component';
+import { TagInputModule } from 'projects/shared/src/lib/components/tag-input/tag-input.module';
 import { MaterialDetailComponent } from './components/material-detail/material-detail.component';
+import { LoadingBarModule } from 'projects/shared/src/lib/components/loading-bar/loading-bar.module';
 import { GuiaCadastroFormComponent } from './components/guia-cadastro-form/guia-cadastro-form.component';
 import { ConsultaContainerComponent } from './containers/consulta-container/consulta-container.component';
 import { GuiaCadastroContainerComponent } from './containers/guia-cadastro-container/guia-cadastro-container.component';
 import { DocumentoCadastroFormComponent } from './components/documento-cadastro-form/documento-cadastro-form.component';
-import { ConsultaMateriaisRoutingModule } from './consulta-materiais-routing.module';
 import { DocumentoCadastroContainerComponent } from './containers/documento-cadastro-container/documento-cadastro-container.component';
 
-import { CardModule } from 'primeng/card';
 import { TagModule } from 'primeng/tag';
+import { CardModule } from 'primeng/card';
 import { MenuModule } from 'primeng/menu';
 import { ToastModule } from 'primeng/toast';
 import { ChipsModule } from 'primeng/chips';
@@ -25,7 +25,6 @@ import { EditorModule } from 'primeng/editor';
 import { DialogModule } from 'primeng/dialog';
 import { DividerModule } from 'primeng/divider';
 import { AccordionModule } from 'primeng/accordion';
-
 import { ScrollTopModule } from 'primeng/scrolltop';
 import { FileUploadModule } from 'primeng/fileupload';
 import { InputTextareaModule } from 'primeng/inputtextarea';
