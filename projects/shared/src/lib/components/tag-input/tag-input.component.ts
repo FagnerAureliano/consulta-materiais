51febc851bee07ef9f539e49de980d401ae4b2f5
FagnerAureliano <fagner2aureliano@gmail.com>
2023-07-26 10:03:44 -0300

    :art: improve tags to insert by pattern

--- projects/shared/src/lib/components/tag-input/tag-input.component.ts
+++ projects/shared/src/lib/components/tag-input/tag-input.component.ts
@@ -6,11 +6,9 @@ import {
   OnChanges,
   OnInit,
   Output,
-  SimpleChanges,
-  ViewChild,
+  ViewChild
 } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { Observable, of } from 'rxjs';
 import Tagify from '@yaireo/tagify';
 
 @Component({
@@ -25,8 +23,6 @@ export class TagInputComponent implements OnInit, OnChanges {
   @Input() whitelist: string[] = [];
 
   tagify: Tagify = null;
-
-  constructor() {}
 
   ngOnChanges(): void {
     if (this.whitelist) {
@@ -43,26 +39,29 @@ export class TagInputComponent implements OnInit, OnChanges {
     this.tagify = new Tagify(inputElement, {
       enforceWhitelist: false,
       whitelist: [],
+      pattern: /^[a-zA-Z0-9\s-_]+$/, // Expressão regular para permitir apenas caracteres alfanuméricos, espaços, hifens e sublinhados
       dropdown: {
         enabled: 1,
-        maxItems: Infinity,
+        maxItems: 4,
         closeOnSelect: false,
       },
       callbacks: {
-        add: (e) => {
+        add: () => {
           tagsControl.setValue(this.tagify.value.map((tag) => tag.value));
         },
-        remove: (e) => {
+        remove: () => {
           tagsControl.setValue(this.tagify.value.map((tag) => tag.value));
         },
-        input: (e) => {
+        input: (e: { detail: { value: string; }; }) => {
           const inputValue = e.detail.value.trim();
           if (inputValue) {
             this.characterInputed.emit(inputValue);
+            this.tagify.state.hasInvalidTag = false;
           }
         },
       },
     });
+    this.tagify.on('invalid', (_) => (this.tagify.state.hasInvalidTag = true));
 
     tagsControl.valueChanges.subscribe(() => {
       this.tagsEmitter.emit(tagsControl.value);
