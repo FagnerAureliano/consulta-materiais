ac9f38f0f7449a5b1ecefb3cb4cfeddd1d604717
FagnerAureliano <fagner2aureliano@gmail.com>
2023-09-21 16:22:09 -0300

    Fix(document):updates

--- projects/consult-materials/src/app/modules/consulta-materiais/components/documento-cadastro-form/documento-cadastro-form.component.ts
+++ projects/consult-materials/src/app/modules/consulta-materiais/components/documento-cadastro-form/documento-cadastro-form.component.ts
@@ -1,5 +1,17 @@
+<<<<<<< Updated upstream
 import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+=======
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { FormGroup, Validators } from '@angular/forms';
+>>>>>>> Stashed changes
 import { FileUpload } from 'primeng/fileupload';
 import { ConsultaMateriaisService } from 'projects/consult-materials/src/app/services/consulta-materiais.service';
 import { Observable, of } from 'rxjs';
@@ -44,7 +56,43 @@ export class DocumentoCadastroFormComponent implements OnInit {
     }
     console.log(this.form.value);
   }
+<<<<<<< Updated upstream
   onClear() {
+=======
+
+  ngOnInit(): void {
+    if (this.material) {
+      this.onFillForm();
+    } else {
+      //TEMPORARY
+      setTimeout(() => {
+        this.onFillForm();
+      }, 300);
+    }
+  }
+
+  onFillForm(): void {
+    if (this.material) {
+      this.hasDocuments = true;
+
+      const materialScope = this.scopes.find(
+        (res) => res.scope === this.material.properties['dc:source']
+      );
+
+      this.form.get('nuxeoPathId').setValue(materialScope.id);
+      this.form.get('title').setValue(this.material.title);
+      this.form
+        .get('description')
+        .setValue(this.material.properties['dc:description']);
+
+      // Devido ao'Tag Input' ser do Shared, utilizando uma lib externa,
+      // é necessário enviar a lista da tag para o mesmo e ser tratada por lá.
+      this._changedTags = this.material.properties['nxtag:tags'];
+    }
+  }
+
+  onClear(): void {
+>>>>>>> Stashed changes
     this.form.get('document').setValue(null);
   }
   handleSearchTags(data: string | string[]): void {
--- projects/consult-materials/src/app/modules/consulta-materiais/components/guia-cadastro-form/guia-cadastro-form.component.ts
+++ projects/consult-materials/src/app/modules/consulta-materiais/components/guia-cadastro-form/guia-cadastro-form.component.ts
@@ -12,11 +12,41 @@ export class GuiaCadastroFormComponent implements OnInit {
   constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+<<<<<<< Updated upstream
     this.form = this.fb.group({
       guiaDocument: [''],
     });
     this.form.get('guiaDocument').valueChanges.subscribe((data) => {
       console.log(data);
     });
+=======
+    if (this.material) {
+      this.onFillForm();
+    } else {
+      //TEMPORARY
+      setTimeout(() => {
+        this.onFillForm();
+      }, 300);
+    }
+  }
+  onFillForm(): void {
+    if (this.material) {
+      this.hasDocuments = true;
+
+      const materialScope = this.scopes.find(
+        (res) => res.scope === this.material.properties['dc:source']
+      );
+      this.form.get('nuxeoPathId').setValue(materialScope.id);
+      this.form.get('title').setValue(this.material.title);
+      this.form.get('content').setValue(this.material.properties['note:note']);
+      this.form
+        .get('description')
+        .setValue(this.material.properties['dc:description']);
+
+      // Devido ao'Tag Input' ser do Shared, utilizando uma lib externa,
+      // é necessário enviar a lista da tag para o mesmo e ser tratada por lá.
+      this._changedTags = this.material.properties['nxtag:tags'];
+    }
+>>>>>>> Stashed changes
   }
 }
