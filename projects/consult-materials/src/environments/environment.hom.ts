ec818ad1437353a720e8c96442a336e0f749183a
luanlbn <nf.luanlbn@fab.mil.br>
2023-10-27 10:01:22 -0300

    Update environment files

--- projects/consult-materials/src/environments/environment.hom.ts
+++ projects/consult-materials/src/environments/environment.hom.ts
@@ -9,9 +9,9 @@ export const environment = {
   KEYCLOAK_REALM: 'FAB',
   KEYCLOAK_CLIENT_ID: 'material-apoio-api',
 
-  FAQ_API_ENDPOINT: 'https://portaldeapoio.hml.rancher.ccarj.intraer/faq-service/api/v1',
   SEARCH_FRONT_URL: 'https://portaldeapoio.hml.rancher.ccarj.intraer/consult-materials/#/',
   USER_API_ENDPOINT: 'https://portaldeapoio.hml.rancher.ccarj.intraer/user-service/api/v1',
-  STREAM_API_ENDPOINT: 'https://portaldeapoio.hml.rancher.ccarj.intraer/stream-service/api/v1',
   SEARCH_API_ENDPOINT: 'https://portaldeapoio.hml.rancher.ccarj.intraer/search-service/api/v1',
+  STREAM_API_ENDPOINT: 'https://portaldeapoio.hml.rancher.ccarj.intraer/stream-service/api/v1',
+  FAQ_API_ENDPOINT: 'https://portaldeapoio.hml.rancher.ccarj.intraer/faq-service/api/v1',
 };
