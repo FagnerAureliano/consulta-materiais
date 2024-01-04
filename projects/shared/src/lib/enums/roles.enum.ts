89f0bb5c4a827907f8e6d8cf612844594e837c96
FagnerAureliano <fagner2aureliano@gmail.com>
2024-01-04 13:28:17 -0300

    :construction:(core): simple adjusts roles, auth and filter

--- projects/shared/src/lib/enums/roles.enum.ts
+++ projects/shared/src/lib/enums/roles.enum.ts
@@ -1,7 +1,7 @@
 export enum NameByRole {
   'MATERIAL_APOIO_USER' = 'Usuário do Consulta Material de Apoio',
-  'MATERIAL_APOIO_ADMIN' = 'Administrador do sistema',
-  'MATERIAL_APOIO_MANAGER' = 'Gerente do sistema',
+  'ROLE_MATERIAL_APOIO_ADMIN' = 'Administrador do sistema',
+  'ROLE_MATERIAL_APOIO_MANAGER' = 'Gerente do sistema',
 
   'MATERIAL_APOIO_SAUDE' = 'Usuário Saúde',
   'MATERIAL_APOIO_EDUCACAO' = 'Usuário Educação',
@@ -11,8 +11,8 @@ export enum NameByRole {
 
 export enum Role {
   'USER' = 'MATERIAL_APOIO_USER',
-  'ADMIN' = 'MATERIAL_APOIO_ADMIN',
-  'MANAGER' = 'MATERIAL_APOIO_MANAGER',
+  'ADMIN' = 'ROLE_MATERIAL_APOIO_ADMIN',
+  'MANAGER' = 'ROLE_MATERIAL_APOIO_MANAGER',
 
   'SAUDE' = 'MATERIAL_APOIO_SAUDE',
   'EDUCACAO' = 'MATERIAL_APOIO_EDUCACAO',
